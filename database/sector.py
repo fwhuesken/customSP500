9e92ff79b3124e43b4fced504fb6dc6f74630272
Felix Huesken <fwhuesken@gmail.com>
2021-09-16 20:04:47 +0000

    sector table actually works...

--- database/sector.py
+++ database/sector.py
@@ -2,7 +2,7 @@ import sqlite3
 import pandas as pd
 
 # load data
-df = pd.read_csv('etf/sp500.csv')
+df = pd.read_csv('etf/sector.csv')
 
 # strip whitespace from headers
 df.columns = df.columns.str.strip()
@@ -10,6 +10,6 @@ df.columns = df.columns.str.strip()
 con = sqlite3.connect("app.db")
 
 # drop data into database
-df.to_sql('sp500', con)
+df.to_sql('sector', con)
 
 con.commit()
